03dfcf7a31e5c5b438225c249963d9b10f5392d4
Sam Soffes <sam@soff.es>
2013-09-13 16:41:50 -0700

    Fight with tests

--- Tests/SAMGradientViewTests.m
+++ Tests/SAMGradientViewTests.m
@@ -12,7 +12,7 @@
 #import "SAMGradientView.h"
 
 @interface SAMGradientViewTests : XCTestCase
-@property (nonatomic, strong) SAMGradientView *gradientView;
+@property (nonatomic) SAMGradientView *gradientView;
 @end
 
 @implementation SAMGradientViewTests
@@ -22,7 +22,7 @@
 - (void)setUp {
 	[super setUp];
 	self.gradientView = [[SAMGradientView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 10.0f, 10.0f)];
-	self.gradientView.backgroundColor = [UIColor blueColor];
+	self.gradientView.backgroundColor = [UIColor colorWithRed:1.0f green:1.0f blue:1.0f alpha:1.0f];
 }
 
 
@@ -31,6 +31,7 @@
 	XCTAssertEqualObjects(self.gradientView.backgroundColor, [self colorAtPoint:CGPointMake(0.0f, 1.0f)], @"");
 
 	self.gradientView.topBorderColor = [UIColor blueColor];
+	XCTAssertEqualObjects(self.gradientView.topBorderColor, [self colorAtPoint:CGPointMake(0.0f, 0.0f)], @"");
 	XCTAssertEqualObjects(self.gradientView.topInsetColor, [self colorAtPoint:CGPointMake(0.0f, 1.0f)], @"");
 }
 
@@ -52,6 +53,7 @@
 	XCTAssertEqualObjects(self.gradientView.backgroundColor, [self colorAtPoint:CGPointMake(0.0f, 8.0f)], @"");
 
 	self.gradientView.bottomBorderColor = [UIColor blueColor];
+	XCTAssertEqualObjects(self.gradientView.bottomBorderColor, [self colorAtPoint:CGPointMake(0.0f, 9.0f)], @"");
 	XCTAssertEqualObjects(self.gradientView.bottomInsetColor, [self colorAtPoint:CGPointMake(0.0f, 8.0f)], @"");
 }
 
@@ -65,30 +67,14 @@
 #pragma mark - Utilities
 
 - (UIColor *)colorAtPoint:(CGPoint)point {
-	// Get image
-	UIGraphicsBeginImageContextWithOptions(self.gradientView.bounds.size, self.gradientView.opaque, 1.0f);
-    [self.gradientView.layer renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-
-	// Get the pixel at a given point
-	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-	unsigned char *data = malloc(image.size.height * image.size.width * 4);
-	NSUInteger bytesPerPixel = 4;
-	NSUInteger bytesPerRow = bytesPerPixel * image.size.width;
-	NSUInteger bitsPerComponent = 8;
-	CGContextRef context = CGBitmapContextCreate(data, image.size.width, image.size.height, bitsPerComponent, bytesPerRow, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-	CGColorSpaceRelease(colorSpace);
-	CGContextDrawImage(context, CGRectMake(0, 0, image.size.width, image.size.height), image.CGImage);
-	CGContextRelease(context);
-	int index = (bytesPerRow * point.y) + point.x * bytesPerPixel;
-
-  
-	// Create and return the color
-  UIColor *result = [UIColor colorWithRed:(CGFloat)data[index] / 255.0f green:(CGFloat)data[index + 1] / 255.0f blue:(CGFloat)data[index + 2] / 255.0f alpha:(CGFloat)data[index + 3] / 255.0f];
-	free(data);
-
-	return result;
+	unsigned char pixel[4] = {0};
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(pixel, 1, 1, 8, 4, colorSpace, (CGBitmapInfo)kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGContextTranslateCTM(context, -point.x, -point.y);
+    [self.gradientView.layer renderInContext:context];
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    return [UIColor colorWithRed:pixel[0]/255.0 green:pixel[1]/255.0 blue:pixel[2]/255.0 alpha:pixel[3]/255.0];
 }
 
 @end
